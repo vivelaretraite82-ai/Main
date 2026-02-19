a5b1e29b6b1dfadca7cba42dd2965638ba7133d2
vivelaretraite82-ai <vivelaretraite82@gmail.com>
2026-02-19 22:02:45 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -125,8 +125,8 @@ app.use(cors({
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
+app.use(express.json({ limit: '50mb' }));
 
 app.use(express.static(__dirname));
 
